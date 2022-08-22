b3f5598d38d2d29bc0c343a346c8dd9598ec5192
Mason Thieman <masonthieman@gmail.com>
2022-08-22 10:32:31 -0500

    Added phase 3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
 ﻿using System;
 
-Console.Write("Guess a secret number: ");
-int guessedNumber = int.Parse(Console.ReadLine());
-Console.WriteLine($"Your guessed number is: {guessedNumber}");
+int secretNumber = 42;
+for (int i=0; i< 4; i++) 
+{
+    Console.Write("Guess a secret number: ");
+    int guessedNumber = int.Parse(Console.ReadLine());
+
+    if (guessedNumber == secretNumber)
+        {
+            Console.WriteLine("Your guess is correct!");
+        }
+
+    else 
+        {
+            Console.WriteLine("Unfortunately, your guess is incorrect.");
+        }
+}
+
+
+
