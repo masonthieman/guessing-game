249b4b43a9feb1e013d0be8363533bc78f2c6618
Mason Thieman <masonthieman@gmail.com>
2022-08-22 10:46:41 -0500

    Completed phase 4

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 
 int secretNumber = 42;
-for (int i=0; i< 4; i++) 
+int attempt = 1;
+bool guessed = false;
+while (attempt < 5 && !guessed) 
 {
-    Console.Write("Guess a secret number: ");
+    Console.Write($"Guess a secret number (Guess #{attempt}): ");
     int guessedNumber = int.Parse(Console.ReadLine());
 
     if (guessedNumber == secretNumber)
         {
             Console.WriteLine("Your guess is correct!");
+            guessed = true;
         }
 
     else 
         {
             Console.WriteLine("Unfortunately, your guess is incorrect.");
         }
+        attempt++;
 }
 
 
